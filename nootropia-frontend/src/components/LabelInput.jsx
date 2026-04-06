a6a56ac69eb8654c6f5dd0231a72c8e6d7180bbb
Victor Galusca <victorgalouska@gmail.com>
2026-04-06 14:04:44 +0300

    feat: Show Password button for Password LabelInput Component

--- nootropia-frontend/src/components/LabelInput.jsx
+++ nootropia-frontend/src/components/LabelInput.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
 
 function LabelInput({
   label,
@@ -8,6 +9,7 @@ function LabelInput({
   onChange,
 }) {
   const [error, setError] = useState("");
+  const [visible, setVisible] = useState(false);
 
   const validate = (value) => {
     if (type === "email") {
@@ -32,19 +34,32 @@ function LabelInput({
     }
   };
 
+  const inputType =
+    type === "password" ? (visible ? "text" : "password") : type;
+
   return (
     <div className="flex flex-col justify-center gap-2">
       <label className="text-gray-100">{label}</label>
-      <input
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={(e) => {
-          validate(e.target.value);
-          onChange(e);
-        }}
-        className="w-full bg-gray-300 hover:bg-gray-500 text-gray-600 hover:text-gray-900 font-semibold py-3 px-6 rounded-lg transition duration-200"
-      />
+      <div className="relative">
+        <input
+          type={inputType}
+          placeholder={placeholder}
+          value={value}
+          onChange={(e) => {
+            validate(e.target.value);
+            onChange(e);
+          }}
+          className="w-full bg-gray-300 hover:bg-gray-500 text-gray-600 hover:text-gray-900 font-semibold py-3 px-6 rounded-lg transition duration-200"
+        />
+        {type === "password" && (
+          <div
+            className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-600"
+            onClick={() => setVisible((prev) => !prev)}
+          >
+            {visible ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+          </div>
+        )}
+      </div>
       {error && <p className="text-red-400 text-sm">{error}</p>}
     </div>
   );
